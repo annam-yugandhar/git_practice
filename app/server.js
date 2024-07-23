148c7db00a48a633824e64ed2394531bb690f97b
yugandhar.Dst <65730028+annam-yugandhar@users.noreply.github.com>
2024-07-23 08:15:35 +0530

    remote changes in server.js
    two developers are trying to changes same code

--- app/server.js
+++ app/server.js
@@ -13,7 +13,7 @@ logger.info('Some new entries in my-index');
 logger.info('another line');
 logger.info('index management');
 logger.info('Update your Elasticsearch indices');
-logger.info('you should see all these in new branch merge');
+logger.info('you should see all these in remote changes');
 logger.info('you should see all these in promethus');
 app.listen(3000, function () {
     logger.info("app listening on port 3000!");
